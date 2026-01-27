7361b2817dcd42be0c36c2c5eb88f68025f5eab3
klim4ik21 <xxlv.klim@yandex.ru>
2026-01-28 00:43:41 +0300

    Add production domains to CORS

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,9 +2,18 @@ package middleware
 
 import "net/http"
 
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173": true,
+	"https://joinbla.ru":    true,
+	"https://www.joinbla.ru": true,
+}
+
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		origin := r.Header.Get("Origin")
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
